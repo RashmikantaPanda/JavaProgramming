111ded57e0045f2bac52b268a8ab9649c7bee326
RashmikantaPanda <rashmikantapanda1234@gmail.com>
2023-04-23 17:27:13 +0530

    LinkedList example

--- src/com/java/collections/linkedList/LInkedListExample.java
+++ src/com/java/collections/linkedList/LInkedListExample.java
@@ -1,6 +1,8 @@
 package com.java.collections.linkedList;
 
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class LInkedListExample {
     public static void main(String[] args) {
@@ -26,15 +28,123 @@ public class LInkedListExample {
         list1.add("Sumanta");
         list1.add("Rahul");
 
+
+
         //addAll(Collection c)
         list.addAll(list1);
         System.out.println(list);
+        System.out.println("----------------------");
 
 
+        //addFirst()
+        list.addFirst("Subham");
+        //addLast()
+        list.addLast("Sukumar");
+        System.out.println(list);
+        System.out.println("----------------------");
 
 
+        //clone()
+        LinkedList linkedList;
+//        Object o=list.clone();
+        linkedList=(LinkedList) list.clone();
+        System.out.println(linkedList);
+        System.out.println("----------------------");
 
 
+        //contains()
+        System.out.println(list.contains("Rashmi"));
+        System.out.println(list.contains("Dinesh"));
+        System.out.println("----------------------");
+
+
+        //Iterator descendingIterator()
+        Iterator<String> iterator=list.descendingIterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+        System.out.println("----------------------");
+
+
+        //element()
+        String str=list.element();
+        System.out.println("Retrieve "+str);
+        System.out.println("----------------------");
+
+
+        //getFirst()  getLast
+        System.out.println(list.getFirst());
+        System.out.println(list.getLast());
+        System.out.println("----------------------");
+
+
+        //indexOf()
+        System.out.println(list.indexOf("Rashmi"));
+        System.out.println(list.indexOf("Subham"));
+        System.out.println("----------------------");
+
+
+        //listIterator()
+        ListIterator<String> listIterator=list.listIterator(5);
+        while (listIterator.hasNext()) {
+            System.out.print(listIterator.next() + " ");
+        }
+        System.out.println();
+        System.out.println("----------------------");
+
+
+        //offer(), offerLast(), offerFirst()
+        list.offer("Sarmila");
+        list.offerLast("Niru");
+        list.offerFirst("Kusum");
+        System.out.println(list);
+        System.out.println("----------------------");
+
+
+        //peek(), peekFirst(), peekLast()
+        String s=list.peek();
+        System.out.println(s);
+        System.out.println(list.peekLast());
+        //System.out.println(list);
+        System.out.println("----------------------");
+
+
+        //poll(), pollFirst(), pollLast()
+        System.out.println(list.poll());
+        System.out.println(list.pollFirst());
+        System.out.println(list.pollLast());
+        System.out.println("----------------------");
+
+
+        //remove(), remove(int index), remove(Object o), removeLast()
+        System.out.println(list);
+        System.out.println(list.remove());
+        System.out.println(list.remove(0));
+        System.out.println(list.remove("Jyoti"));
+        System.out.println(list.removeLast());
+        System.out.println(list);
+        System.out.println("----------------------");
+
+
+        //set(int index, E element)
+        System.out.println(list.set(0,"Rashmi"));
+        System.out.println("----------------------");
+
+
+        //toArray()
+        Object[] objArr= list.toArray();
+        for(Object o:objArr){
+            System.out.println(o);
+        }
+        System.out.println("----------------------");
+
+        //size()
+        System.out.println(list.size());
+        System.out.println("----------------------");
+
+        //clear()
+        list.clear();
+        System.out.println(list);
 
     }
 }
