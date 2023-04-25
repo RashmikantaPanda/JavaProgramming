e19c9425cdb10d5f306e9a5f8c107682306b7732
RashmikantaPanda <rashmikantapanda1234@gmail.com>
2023-04-25 21:49:02 +0530

    LinkedList Queue

---
+++ src/com/java/collections/queue/LinkedListEx.java
@@ -0,0 +1,27 @@
+package com.java.collections.queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LinkedListEx{
+    public static void main(String[] args) {
+        Queue<Integer> q= new LinkedList<Integer>();
+
+        //add elements to the queue
+        q.add(100);
+        q.add(200);
+        q.add(50);
+        q.add(150);
+
+        System.out.println("Elements of the queue are : ");
+        System.out.println(q);
+
+        //element(), returns the head(first) of the queue
+        System.out.println(q.element());
+
+        //poll, removes and returns the head of the queue
+        System.out.println(q.poll());
+        System.out.println("Now the q is : "+q);
+
+    }
+}
