a1aadd1c96189d11a5dd72a73cc0bc64e4aa0b30
RashmikantaPanda <rashmikantapanda1234@gmail.com>
2023-05-03 22:18:04 +0530

    OccurrenceOfEachNo

--- src/com/java/questions/OccurrenceOfInteger.java
+++ src/com/java/questions/OccurrenceOfInteger.java
@@ -11,7 +11,36 @@ import java.util.Map;
 
 public class OccurrenceOfInteger {
     public static void main(String[] args) {
+        System.out.println("No of Occurrence of Each Numbers\n");
         int[] arr={2,2,1,3,2,1,2,3,5,2,4,6,5,8,9,5,7,5,6,5,4,2,1,2,3,2,1,2,5,4,8,6,5,4};
+        System.out.println(Arrays.toString(arr));
+        usingNativeMethod(arr);
+        usingMap(arr);
+
+    }
+    public static void usingNativeMethod(int[] arr) {
+        int [] fr = new int [arr.length];
+        int visited = -1;
+        for(int i = 0; i < arr.length; i++){
+            int count = 1;
+            for(int j = i+1; j < arr.length; j++){
+                if(arr[i] == arr[j]){
+                    count++;
+                    //To avoid counting same element again
+                    fr[j] = visited;
+                }
+            }
+            if(fr[i] != visited)
+                fr[i] = count;
+        }
+
+        System.out.println(" ---Using Native Method--- ");
+        for (int i=0; i<fr.length; i++){
+            if(fr[i]!=-1)
+                System.out.println(arr[i]+" ---> "+fr[i]+" times.");
+        }
+    }
+    public static void usingMap(int[] arr) {
         Map<Integer, Integer> map=new HashMap<>();
 
         for (int j : arr) {
@@ -22,9 +51,9 @@ public class OccurrenceOfInteger {
             }
         }
 
-        System.out.println(Arrays.toString(arr));
+        System.out.println(" ---Using Collections(Map)--- ");
         for(Map.Entry<Integer, Integer> entry:map.entrySet()){
-            System.out.println(entry.getKey()+" is "+entry.getValue()+" times.");
+            System.out.println(entry.getKey()+" ---> "+entry.getValue()+" times.");
         }
     }
 }
